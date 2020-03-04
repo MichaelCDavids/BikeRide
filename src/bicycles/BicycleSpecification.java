61273206827d344906b3d872619e3d7d05568c8e
Michael Davids <michaelcdavids@gmail.com>
2020-03-04 19:25:12 +0200

    added bicycle type to BicycleSpecification class with getter

--- src/bicycles/BicycleSpecification.java
+++ src/bicycles/BicycleSpecification.java
@@ -3,10 +3,12 @@ package bicycles;
 public class BicycleSpecification {
     private int accelerationSpeed;
     private int brakeSpeed;
+    private Bicycle bicycleType;
 
-    public BicycleSpecification(int accelerationSpeed, int brakeSpeed){
+    public BicycleSpecification(int accelerationSpeed, int brakeSpeed, Bicycle bicycle){
         this.accelerationSpeed = accelerationSpeed;
         this.brakeSpeed = brakeSpeed;
+        this.bicycleType = bicycle;
     }
 
 
@@ -16,5 +18,9 @@ public class BicycleSpecification {
 
     public int getBrakeSpeed() {
         return brakeSpeed;
+    }
+
+    public Bicycle getBicycleType() {
+        return bicycleType;
     }
 }
