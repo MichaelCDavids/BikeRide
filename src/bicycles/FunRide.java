472ed4938dceecdef8edfe323421224dbbd3ad42
Michael Davids <michaelcdavids@gmail.com>
2020-03-04 19:36:40 +0200

    imported ArrayList class for creating a list of bikes entered

--- src/bicycles/FunRide.java
+++ src/bicycles/FunRide.java
@@ -1,8 +1,12 @@
 package bicycles;
 
+import java.util.ArrayList;
+
 public class FunRide {
 
     private int maximumNumberOfBikesForRide;
+
+    private ArrayList<Bicycle> listOfBikesEntered = new ArrayList();
 
     public FunRide(int maximumNumberOfBikes){
         this.maximumNumberOfBikesForRide = maximumNumberOfBikes;
